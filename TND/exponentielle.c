8555652be6f81a7dfbe70baf725e9671511acf42
Alex5060 <36207171+Alex5060@users.noreply.github.com>
2018-04-21 13:10:16 +0200

    Implémentation calcul Intervalle (Sam) et Exponentielle (Romain)

---
+++ TND/exponentielle.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdio.h>
+
+float exponentielle(int nbIterationsTaylor, float argument);
+
+float exponentielle(int nbIterationsTaylor, float argument)
+{
+	float termeTotal = 1;
+	int degreIteration;
+
+	for (degreIteration = 1; degreIteration < nbIterationsTaylor; degreIteration++) {
+		float terme = 1;
+		int iteration = 0;
+
+		for (int i = 0; i < iteration; i++)
+		{
+			terme *= argument;
+		}
+
+		float diviseur = factorielle(nbIterationsTaylor);
+		termeTotal += terme / diviseur;
+	}
+	return termeTotal;
+}
