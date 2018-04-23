5bc8fcfb5d26f265feea066ef577bc8a27d68e8c
Alex5060 <36207171+Alex5060@users.noreply.github.com>
2018-04-23 11:41:24 +0200

    add simpson.obj

---
+++ biblio_Erreur/erreur.h
@@ -0,0 +1,6 @@
+#pragma once
+#include <stdio.h>
+# include <stdbool.h>
+#include <math.h>
+bool verifDecimales(double x1, double x2, int nbDecimales);
+void afficheDecimales(double x, int nbDecimales);
