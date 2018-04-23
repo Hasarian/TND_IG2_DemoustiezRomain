0d077fa83d7efad4ab91fd3088bf1b971ccc5bdd
hasarian <romain.demoustiez@gmail.com>
2018-04-23 12:23:21 +0200

    table_cstr + valeurAlpha

--- TND/detectionAnomalie.h
+++ TND/detectionAnomalie.h
@@ -2,9 +2,15 @@
 #include <stdio.h>
 #include <math.h>
 #include <string.h>
+#include "../biblioInteg/simpson.h"
 
-#define LIGNEMAX 39
-#define COLONNEMAX 9
+#define LIGNEMAX 40
+#define COLONNEMAX 10
+
+#define DOUBLETOINT 10000
+typedef int TypeTable;
 
 float calculIntervalle(float moyenne, float variance, int n, float coefficiantAlpha, float *upperLimit);
 float traitementBaseModele(int n, float lowerControlLimit, float upperControlLimit, float lowerWarningLimit, float upperWarningLimit, float dataAEvaluer);
+double* table_cstr(Fonction f);
+double valeurAlpha(double alpha, TypeTable* tableNormale[]);
--- TND/table_cstr.c
+++ TND/table_cstr.c
@@ -1,1 +1,23 @@
 #include "detectionAnomalie.h"
+
+double* table_cstr(Fonction f)
+{
+	int nbPoints = 3;
+	double dernierResultat = 0.5;
+	double tabNormale[LIGNEMAX][COLONNEMAX];
+	tabNormale[0][0] = dernierResultat;
+	double bInf = 0;
+	double bSup,resultat;
+	for (int iLigne = 0; iLigne < LIGNEMAX; iLigne++)
+	{
+		for (int iColonne = 0; iColonne < COLONNEMAX; iColonne++)
+		{
+			bSup = iLigne / 10 + iColonne / 100;
+			resultat = calculSimpson(nbPoints,bInf,bSup,f);
+			dernierResultat += resultat;
+			tabNormale[iLigne][iColonne] = resultat*DOUBLETOINT;
+			bInf = bSup;
+		}
+	}
+	return tabNormale;
+}
