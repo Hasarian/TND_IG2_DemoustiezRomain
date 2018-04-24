a3bc4fce969bdb758237d6970c0dd88ceb196346
Alex5060 <36207171+Alex5060@users.noreply.github.com>
2018-04-24 13:19:10 +0200

    update merge merde

--- TND/power.c
+++ TND/power.c
@@ -18,66 +18,6 @@ void main(void) {
 }
 */
 
-<<<<<<< HEAD
-//void main(void) {
-//	bool negatif = false;
-//	double a;	
-//	double x;
-//	int nbDecimales;
-//	double argument;
-//	int nbLimites;
-//	double reste;
-<<<<<<< HEAD
-=======
-//	int nbIterations;
-//	double res;
->>>>>>> 9f61685b938f9d360309e4426ba3fca37474ccac
-//
-//	do {
-//		printf("Pour a^x -> veuillez entrer le a : ");
-//		scanf_s("%ld", &a);		// a ne peut pas être négatif
-//		vider();
-//	} while (a < 0);
-//
-//	printf("Pour a^x -> veuillez entrer le x : ");
-//	scanf_s("%ld", &x);
-//	vider();
-//
-//	if (x < 0) {
-<<<<<<< HEAD
-//		x *= 1;
-=======
-//		x *= -1;
->>>>>>> 9f61685b938f9d360309e4426ba3fca37474ccac
-//		negatif = true;
-//	}
-//
-//	do {
-//		printf("Pour la précision, veuillez entre un nombre positif de décimales : ");
-//		scanf_s("%d", &nbDecimales);
-//		vider();
-//	} while (nbDecimales < 0);
-//
-//	argument = x * log(a);
-//
-//	nbLimites = (int)argument / LIMITE;
-//	reste = argument - nbLimites * LIMITE;
-//
-<<<<<<< HEAD
-//	int nbIterations = degre(nbDecimales, nbLimites);
-//	double res = resultat(reste, nbLimites, nbIterations);
-=======
-//	nbIterations = degre(nbDecimales, nbLimites);
-//	res = resultat(reste, nbLimites, nbIterations);
->>>>>>> 9f61685b938f9d360309e4426ba3fca37474ccac
-//
-//	if (negatif)
-//		res = 1 / res;
-//
-//	printf("Resultat : %.5lf", res);
-//
-//}
-=======
 void main(void) {
 	bool negatif = false;
 	double a;	
@@ -124,7 +64,6 @@ void main(void) {
 	printf("Resultat : %.5lf", res);
 
 }
->>>>>>> b99960da3308388137ef29e51e5d3205044cccfd
 
 int degre(int nbDecimales, int nbLimites) {
 	double limiteAugmentée;
